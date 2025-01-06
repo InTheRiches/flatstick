fd9d3d5c64fe7040a4f7eddd98ecdef1f68c4796
InTheRiches <intherichesftk@gmail.com>
2025-01-06 17:34:46 -0500

    fix(home): Temp removed notification btn

--- components/tabs/home/Header.js
+++ components/tabs/home/Header.js
@@ -1,8 +1,6 @@
 import useColors from "../../../hooks/useColors";
 import {getAuth} from "firebase/auth";
 import {Text, View} from "react-native";
-import {PrimaryButton} from "../../general/buttons/PrimaryButton";
-import Svg, {Path} from "react-native-svg";
 import React from "react";
 
 export function Header() {
@@ -27,16 +25,16 @@ export function Header() {
                     color: colors.text.primary
                 }}>{auth.currentUser.displayName}</Text>
             </View>
-            <PrimaryButton onPress={() => {
+            {/*<PrimaryButton onPress={() => {*/}
 
-            }} style={{borderRadius: 30, aspectRatio: 1, height: 42}}>
-                <Svg width={28} height={28} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     strokeWidth={1.5}
-                     stroke={colors.button.primary.text}>
-                    <Path strokeLinecap="round" strokeLinejoin="round"
-                          d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
-                </Svg>
-            </PrimaryButton>
+            {/*}} style={{borderRadius: 30, aspectRatio: 1, height: 42}}>*/}
+            {/*    <Svg width={28} height={28} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"*/}
+            {/*         strokeWidth={1.5}*/}
+            {/*         stroke={colors.button.primary.text}>*/}
+            {/*        <Path strokeLinecap="round" strokeLinejoin="round"*/}
+            {/*              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>*/}
+            {/*    </Svg>*/}
+            {/*</PrimaryButton>*/}
         </View>
     )
 }
