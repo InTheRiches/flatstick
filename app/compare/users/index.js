9abc633009d0fa69c5a1c89a9d81984660529e04
InTheRiches <intherichesftk@gmail.com>
2025-01-06 17:34:10 -0500

    fix(stats): Fixed weird inconsistencies in styling

--- app/compare/users/index.js
+++ app/compare/users/index.js
@@ -46,34 +46,25 @@ export default function CompareUsers({}) {
                 </Pressable>
                 <Text style={{fontSize: 24, fontWeight: 500, color: colors.text.primary}}>Compare Users</Text>
             </View>
+            <Text style={{color: colors.text.secondary, fontWeight: 600, marginTop: 16, marginBottom: 6}}>USER</Text>
             <View style={{
                 padding: 8,
                 backgroundColor: colors.background.secondary,
                 borderRadius: 14,
                 marginBottom: 8,
-                marginTop: 16,
                 flexDirection: "row",
                 alignItems: "center",
                 justifyContent: "space-between"
             }}>
-                <View style={{flexDirection: "row",}}>
+                <View style={{flexDirection: "row", alignItems: "center"}}>
                     <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={colors.text.secondary} width={48} height={48}>
                         <Path fillRule="evenodd"
                               d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
                               clipRule="evenodd"/>
                     </Svg>
                     <View style={{marginLeft: 6}}>
-                        <Text style={{color: colors.text.primary, fontSize: 16, fontWeight: 500}}>{profile.firstName + " " + profile.lastName}</Text>
-                        <Text style={{color: colors.text.secondary, fontSize: 14}}>SG: {profile.strokesGained}</Text>
+                        <Text style={{color: colors.text.primary, fontSize: 24, fontWeight: 500, marginBottom: 4}}>{profile.firstName + " " + profile.lastName}</Text>
                     </View>
-                </View>
-                <View style={{backgroundColor: colors.button.secondary.background, alignItems: "center", justifyContent: "center", aspectRatio: 1, borderRadius: 24, paddingHorizontal: 8}}>
-                    <Svg width={20} height={20} xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 24 24" strokeWidth={2}
-                         stroke={colors.button.secondary.text} className="size-6">
-                        <Path strokeLinecap="round" strokeLinejoin="round"
-                              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"/>
-                    </Svg>
                 </View>
             </View>
             <Text style={{color: colors.text.secondary, fontWeight: 600, marginTop: 16, marginBottom: 6}}>COMPARISON</Text>
