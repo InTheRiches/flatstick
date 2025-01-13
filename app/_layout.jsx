2e2848cf2a19987e4a2733826b37421beda6f767
InTheRiches <intherichesftk@gmail.com>
2025-01-13 18:14:29 -0500

    fix(production): Attempted some debugging on problem

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -10,6 +10,7 @@ import {BottomSheetModalProvider} from "@gorhom/bottom-sheet";
 import * as SystemUI from "expo-system-ui";
 import {GoogleSignin} from "@react-native-google-signin/google-signin";
 import React from "react";
+import {ErrorBoundary} from "react-error-boundary";
 
 export default function RootLayout() {
   const colors = useColors();
@@ -29,45 +30,63 @@ export default function RootLayout() {
   SystemUI.setBackgroundColorAsync("#FFFFFF");
   Appearance.setColorScheme("light");
 
+  function fallbackRender({ error, resetErrorBoundary }) {
+    // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+    return (
+        <div role="alert">
+          <p>Something went wrong:</p>
+          <pre style={{ color: "red" }}>{error.message}</pre>
+        </div>
+    );
+  }
+
   return (
-      <AppProvider>
-        <StatusBar style="dark" barStyle={"light-content"} backgroundColor={'transparent'} translucent={true} />
-        <GestureHandlerRootView>
-          <BottomSheetModalProvider>
-            <Stack screenOptions={{
-                  headerShown: false,
-                  navigationBarColor: colors.background.primary,
-                }}>
-              <Stack.Screen name="(tabs)"/>
-              <Stack.Screen name={"simulation/pressure/index"}/>
-              <Stack.Screen name={"simulation/pressure/setup/index"}/>
-              <Stack.Screen name={"simulation/round/index"}/>
-              <Stack.Screen name={"simulation/real/index"}/>
-              <Stack.Screen name="+not-found"/>
-              <Stack.Screen name={"editputters/index"} options={{
-                presentation: 'modal',
-                animation: "slide_from_bottom",
-                animationDuration: 150,
-              }}/>
-              <Stack.Screen name={"editgrips/index"} options={{
-                presentation: 'modal',
-                animation: "slide_from_bottom",
-              }}/>
-              <Stack.Screen name={"sessions/index"}/>
-              <Stack.Screen name={"sessions/individual/index"}/>
-              <Stack.Screen name={"settings/stats/index"} options={{
-                presentation: 'modal',
-                animation: "slide_from_bottom",
-                animationDuration: 150,
-              }}/>
-              <Stack.Screen name={"settings/user/index"}/>
-              <Stack.Screen name={"compare/putters/index"} />
-              <Stack.Screen name={"compare/grips/index"} />
-              <Stack.Screen name={"compare/users/search/index"} />
-              <Stack.Screen name={"compare/users/index"} />
-            </Stack>
-          </BottomSheetModalProvider>
-        </GestureHandlerRootView>
-      </AppProvider>
+      <ErrorBoundary
+          fallbackRender={fallbackRender}
+          onReset={(details) => {
+            // Reset the state of your app so the error doesn't happen again
+          }}
+      >
+        <AppProvider>
+          <StatusBar style="dark" barStyle={"light-content"} backgroundColor={'transparent'} translucent={true} />
+          <GestureHandlerRootView>
+            <BottomSheetModalProvider>
+              <Stack screenOptions={{
+                    headerShown: false,
+                    navigationBarColor: colors.background.primary,
+                  }}>
+                <Stack.Screen name="(tabs)"/>
+                <Stack.Screen name={"simulation/pressure/index"}/>
+                <Stack.Screen name={"simulation/pressure/setup/index"}/>
+                <Stack.Screen name={"simulation/round/index"}/>
+                <Stack.Screen name={"simulation/real/index"}/>
+                <Stack.Screen name="+not-found"/>
+                <Stack.Screen name={"editputters/index"} options={{
+                  presentation: 'modal',
+                  animation: "slide_from_bottom",
+                  animationDuration: 150,
+                }}/>
+                <Stack.Screen name={"editgrips/index"} options={{
+                  presentation: 'modal',
+                  animation: "slide_from_bottom",
+                }}/>
+                <Stack.Screen name={"sessions/index"}/>
+                <Stack.Screen name={"sessions/individual/index"}/>
+                <Stack.Screen name={"settings/stats/index"} options={{
+                  presentation: 'modal',
+                  animation: "slide_from_bottom",
+                  animationDuration: 150,
+                }}/>
+                <Stack.Screen name={"settings/user/index"}/>
+                <Stack.Screen name={"compare/putters/index"} />
+                <Stack.Screen name={"compare/grips/index"} />
+                <Stack.Screen name={"compare/users/search/index"} />
+                <Stack.Screen name={"compare/users/index"} />
+              </Stack>
+            </BottomSheetModalProvider>
+          </GestureHandlerRootView>
+        </AppProvider>
+      </ErrorBoundary>
   );
 }
