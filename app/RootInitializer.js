c1ac5108f0d9f2545f85d6787ced1dc200766b6b
InTheRiches <intherichesftk@gmail.com>
2025-01-16 10:38:51 -0500

    feat(all): Removed google analytics, made fully functional no wifi screen.

--- app/RootInitializer.js
+++ app/RootInitializer.js
@@ -4,7 +4,6 @@ import {useRouter} from "expo-router";
 import {useAppContext, useSession} from "../contexts/AppCtx";
 import {AnimatedBootSplash} from "@/components/tabs/home/AnimatedBootSplash";
 import NetInfo from '@react-native-community/netinfo';
-import analytics from '@react-native-firebase/analytics';
 
 export default function RootInitializer({}) {
     const [visible, setVisible] = React.useState(true);
@@ -16,8 +15,6 @@ export default function RootInitializer({}) {
     // Monitor authentication state changes
     useEffect(() => {
         const unsubscribeNetInfo = NetInfo.addEventListener(state => {
-            console.log('Connection type', state.type);
-            console.log('Is connected?', state.isConnected);
             if (!state.isConnected) {
                 router.push({pathname: "/offline"});
             }
@@ -25,8 +22,6 @@ export default function RootInitializer({}) {
 
         const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
-                analytics().logLogin({method: "app"});
-
                 user.getIdToken().then((token) => {
                     setSession(token);
                     initialize();
--- app/offline/index.js
+++ app/offline/index.js
@@ -1,10 +1,40 @@
 import ScreenWrapper from "../../components/general/ScreenWrapper";
-import {Text} from "react-native";
+import {Text, View} from "react-native";
+import React, {useEffect} from "react";
+import useColors from "../../hooks/useColors";
+import {useRoute} from "@react-navigation/native";
+import NetInfo from "@react-native-community/netinfo";
+import {useNavigation} from "expo-router";
 
 export default function Offline() {
+    const colors = useColors();
+    const navigation = useNavigation();
+
+    // Monitor authentication state changes
+    useEffect(() => {
+        const unsubscribeNetInfo = NetInfo.addEventListener(state => {
+            if (state.isConnected) {
+                navigation.goBack();
+            }
+        });
+        return () => {
+            unsubscribeNetInfo();
+        }
+    }, []);
+
     return (
         <ScreenWrapper>
-            <Text>Offline</Text>
+            <View style={{
+                borderBottomWidth: 1,
+                borderBottomColor: colors.border.default,
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                paddingHorizontal: 32
+            }}>
+                <Text style={{color: colors.text.primary, fontSize: 24, fontWeight: 600, textAlign: "center"}}>You are Offline</Text>
+                <Text style={{color: colors.text.secondary, fontSize: 18, marginTop: 12, textAlign: "center"}}>Reconnect to wifi to use Flatstick.</Text>
+            </View>
         </ScreenWrapper>
     )
 }
