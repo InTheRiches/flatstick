09baf4907e2fc45bf75e1406286ce075d29399d7
InTheRiches <intherichesftk@gmail.com>
2025-01-19 22:07:15 -0500

    feat(session): Added confirm delete to individual session

---
+++ components/sessions/individual/ConfirmDelete.js
@@ -0,0 +1,77 @@
+import React, {useCallback} from "react";
+import {Pressable, Text, View} from "react-native";
+import {BottomSheetModal, BottomSheetView} from "@gorhom/bottom-sheet";
+import useColors from "@/hooks/useColors";
+import CustomBackdrop from "@/components/general/popups/CustomBackdrop";
+import {PrimaryButton} from "@/components/general/buttons/PrimaryButton";
+
+export function ConfirmDelete({ confirmDeleteRef, cancel, onDelete}) {
+    const colors = useColors();
+
+    const myBackdrop = useCallback(({animatedIndex, style}) => {
+        return (<CustomBackdrop
+            reference={confirmDeleteRef}
+            animatedIndex={animatedIndex}
+            style={style}
+        />);
+    }, []);
+
+    // renders
+    return (<BottomSheetModal
+        ref={confirmDeleteRef}
+        backdropComponent={myBackdrop}
+        backgroundStyle={{backgroundColor: colors.background.secondary}}
+        keyboardBlurBehavior={"restore"}>
+        <BottomSheetView style={{
+            paddingBottom: 20, backgroundColor: colors.background.secondary,
+        }}>
+            <View style={{
+                paddingHorizontal: 32, flexDirection: "column", alignItems: "center",
+            }}>
+                <View style={{
+                    flexDirection: "row", gap: 12, alignItems: "center", marginBottom: 8,
+                }}>
+                    <View
+                        style={{
+                            height: 32,
+                            aspectRatio: 1,
+                            alignItems: "center",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                            borderRadius: 50,
+                            backgroundColor: colors.button.danger.background,
+                        }}>
+                        <Text style={{color: "white", fontWeight: 600, fontSize: 24}}>!</Text>
+                    </View>
+                    <Text style={{fontSize: 26, fontWeight: 600, color: colors.text.primary, textAlign: "left",}}>
+                        Delete Session
+                    </Text>
+                </View>
+                <Text
+                    style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        color: colors.text.secondary,
+                        textAlign: "center",
+                        width: "70%",
+                        marginBottom: 16,
+                    }}>
+                    Are you sure you want to delete this session? This action cannot be undone.
+                </Text>
+                <View style={{flexDirection: "column", width: "80%", alignItems: "center", justifyContent: "flex-end"}}>
+                    <Pressable onPress={onDelete} style={({pressed}) => [{
+                        backgroundColor: pressed ? colors.button.danger.depressed : colors.button.danger.background,
+                        paddingVertical: 10,
+                        borderRadius: 10,
+                        marginTop: 16,
+                        width: "100%"
+                    }]}>
+                        <Text style={{textAlign: "center", color: colors.button.danger.text, fontWeight: 500}}>Delete Session</Text>
+                    </Pressable>
+                    <PrimaryButton key={"primary"} onPress={cancel} title={"Cancel"}
+                                   style={{marginTop: 10, paddingVertical: 10, borderRadius: 10, width: "100%"}}></PrimaryButton>
+                </View>
+            </View>
+        </BottomSheetView>
+    </BottomSheetModal>);
+}
